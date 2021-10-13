9959c75aa33ab283466156c0e434231687202686
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-10-13 11:58:44 -0400

    Pull init

---
+++ src/Version.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace criativaBase\api\versao;
+
+use criativaBase\Table\versao;
+use criativa\lib\Model;
+
+class ApiVersao extends Model {
+
+    public function __construct(){
+        parent::__construct();
+
+        if (!$this->existsTable('versao')){
+            $table = new versao();
+            $table->dbExecute();
+        }
+    }
+
+    public function listTable(){
+        $a = "./src/base/tabela/";
+
+        $namespace_base = DEFINED('NAMESPACE_BASE') ? NAMESPACE_BASE : 'mvc';
+
+        echo $a;
+        $list = array();
+        if (is_dir($a)) {
+            if ($dh = opendir($a)) {
+                while (($file = readdir($dh)) !== false) {
+                    if ($file!= '.' && $file != '..'){
+                        $class = "\\{$namespace_base}\\base\\tabela\\" . str_replace('.php','',$file);
+                        //require_once "{$a}{$file}";
+                        $exc = new $class();
+
+                        $temp = new \stdClass();
+                        $temp->tabela = get_class($exc);
+                        $temp->current = $this->get($temp->tabela);
+                        $temp->new = $exc->build;
+                        $temp->description = $exc->description;
+                        $temp->important = $exc->important;
+                        $temp->optional = $exc->optional;
+                        $temp->status = ($temp->current == $temp->new);
+
+                        $list[] = $temp;
+                    }
+                }
+                closedir($dh);
+            }
+        }
+
+        return $list;
+    }
+
+    public function get($table){
+        $query = $this->First($this->Select("SELECT build FROM versao WHERE tabela = '{$table}'"));
+        return isset($query->build) ? $query->build : '*';
+    }
+}
