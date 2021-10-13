f59840a4e6d8e321052b6996a7e02070798e88ce
Maicon Takeshi Kamiya <maiconkamiya@criativa.eti.br>
2021-10-13 11:41:41 -0400

    Pull init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3f8172071572969239e189195ae6115bea7af08e',
+    'name' => 'mtakeshi/mvc-versiondb',
+  ),
+  'versions' => 
+  array (
+    'mtakeshi/mvc-framework' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '88076860a33b82e32319c6b6c68e59d6e14163e9',
+    ),
+    'mtakeshi/mvc-versiondb' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3f8172071572969239e189195ae6115bea7af08e',
+    ),
+  ),
+);
