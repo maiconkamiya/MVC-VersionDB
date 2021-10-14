40b7e667d6a7110abaf998dbf3f52e4be41ad6bb
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-10-14 11:07:53 -0400

    Create versao.php

---
+++ src/table/versao.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maicon
+ * Date: 16/02/2018
+ * Time: 15:09
+ */
+namespace criativaBase\table;
+
+use criativaBase\database;
+
+class versao extends DataBase {
+    public function __construct(){
+
+        parent::__construct();
+
+        $this->description = "Tabela versão do banco de dados";
+
+        $this->cmd[] = array('versao','create','',
+            'CREATE TABLE IF NOT EXISTS `versao` (
+              `tabela` varchar(75) NOT NULL,
+              `descricao` text COLLATE utf8_unicode_ci,
+              `dtupdate` datetime DEFAULT NULL,
+              `build` int NOT NULL DEFAULT 0,
+              PRIMARY KEY (`tabela`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
+
+        $this->cmd[] = array('versao','add','tabela','ALTER TABLE `versao` ADD `tabela` varchar(75) NOT NULL FIRST;');
+
+        $this->build = 1;
+    }
+}
