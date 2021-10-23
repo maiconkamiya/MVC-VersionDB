2cfb12e72660c33a0afee99d0af437492c841501
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-10-23 08:55:08 -0400

    Add table log

---
+++ src/table/log_alt_dado.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maicon
+ * Date: 16/02/2018
+ * Time: 15:09
+ */
+namespace criativaBase\table;
+
+use criativaBase\DataBase;
+
+class log_alt_dado extends DataBase {
+    public function __construct(){
+
+        parent::__construct();
+
+        $this->description = "Tabela log alteração de dados";
+
+        $tab_name = self::$prefix . "log_alt_dado";
+
+        $this->cmd[] = array("{$tab_name}",'create','',"CREATE TABLE IF NOT EXISTS `{$tab_name}` (
+              `codusuario` int NOT NULL,
+              `tabela` varchar(75) COLLATE utf8_unicode_ci NOT NULL,
+              `campo` varchar(75) COLLATE utf8_unicode_ci NOT NULL,
+              `valornovo` text COLLATE utf8_unicode_ci NOT NULL,
+              `valoranterior` text COLLATE utf8_unicode_ci NOT NULL,
+              `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+
+        $this->build = 1;
+    }
+}
