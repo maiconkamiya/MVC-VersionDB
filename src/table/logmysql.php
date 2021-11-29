83bf5258845384eb3c280dbbdb331d4ccaf2e3d1
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-11-29 10:30:00 -0400

    pull init

---
+++ src/table/logmysql.php
@@ -0,0 +1,26 @@
+<?php
+namespace criativaBase\table;
+
+use criativaBase\DataBase;
+
+class logmysql extends DataBase {
+    public function __construct(){
+        parent::__construct();
+
+        $this->description = "Tabela log de erro Model";
+
+        $tab_name = self::$prefix . "logmysql";
+
+        $this->cmd[] = array("{$tab_name}",'create','',"CREATE TABLE IF NOT EXISTS `{$tab_name}` (
+              `codlog` int(11) NOT NULL AUTO_INCREMENT,
+              `descricao` text COLLATE utf8_unicode_ci,
+              `sintaxe` text COLLATE utf8_unicode_ci NOT NULL,
+              `data` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+              `dataexc` datetime DEFAULT NULL,
+              PRIMARY KEY (`codlog`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+
+        /*Versão*/
+        $this->build = 2;
+    }
+}
