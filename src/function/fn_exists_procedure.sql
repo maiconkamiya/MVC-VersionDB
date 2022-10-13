262847a35d611f74f9d359e14f3b3e370ab9c024
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2022-10-13 17:55:59 -0400

    fix bug fn_exists_procedure create

--- src/function/fn_exists_procedure.sql
+++ src/function/fn_exists_procedure.sql
@@ -1,8 +1,8 @@
 DROP FUNCTION IF EXISTS `fn_exists_procedure`;
-CREATE FUNCTION `fn_exists_procedure`(in_name VARCHAR(255) COLLATE utf8mb3) RETURNS BIT DETERMINISTIC BEGIN
-    SELECT COUNT(1) INTO @f_result
-    FROM information_schema.ROUTINES as info
-    WHERE info.ROUTINE_SCHEMA = DATABASE() AND info.ROUTINE_TYPE = 'PROCEDURE' AND info.ROUTINE_NAME = in_name;
+CREATE FUNCTION `fn_exists_procedure`(`in_name` VARCHAR(255)) RETURNS BIT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN 
+    SELECT COUNT(1) INTO @f_result 
+    FROM information_schema.ROUTINES as info 
+    WHERE info.ROUTINE_SCHEMA = DATABASE() AND info.ROUTINE_TYPE = 'PROCEDURE' AND info.ROUTINE_NAME = in_name; 
 
-    RETURN @f_result;
+    RETURN @f_result; 
 END;
